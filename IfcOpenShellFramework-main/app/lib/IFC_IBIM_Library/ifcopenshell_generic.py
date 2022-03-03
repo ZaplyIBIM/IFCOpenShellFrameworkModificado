de8bf8b99983973697d7ce046219f29321ef5987
AToradaIBIM <atorada@ibim.es>
2022-03-03 07:59:35 +0100

    REFACTOR - Renaming Variables IfcOpenShell_Generic

--- IfcOpenShellFramework-main/app/lib/IFC_IBIM_Library/ifcopenshell_generic.py
+++ IfcOpenShellFramework-main/app/lib/IFC_IBIM_Library/ifcopenshell_generic.py
@@ -5,28 +5,28 @@ Clase general que se encarga de proporcionar acceso a los diferentes métodos de
 """
 
 from app.lib.IFC_IBIM_Library.geometry import Geometry
-from .ifcfiles import IfcFiles
+from .ifcfiles import ifcfiles
 
 
-class IfcOpenShellGeneric(Geometry):
+class ifcopenshell_generic(Geometry):
   # ctor
   def __init__(self):
     pass;
 
   # methods 
-  def open(self, dataRequest): 
-    ifcExtended = IfcFiles(dataRequest["pathOfFiles"], dataRequest["guidModels"]);
-    return ifcExtended.guidFiles; 
+  def open(self, data_request): 
+    ifcextended = ifcfiles(data_request["pathOfFiles"], data_request["guidModels"]);
+    return ifcextended.guid_files; 
 
-  def close(self, guidModels): 
-    IfcFiles.closeIfcs(guidModels);
+  def close(self, guid_models): 
+    ifcfiles.close_ifcs(guid_models);
 
-  def transformCivilIfc(self,guidModels, bodyJson):
-    modelNames = bodyJson['models'];
-    for modelName in modelNames:
-      fileSearch = IfcFiles.getSpecificFile(guidModels, modelName);
-      if(fileSearch != None):
-        fileSearch.transformCivilFile();          
+  def transformCivilIfc(self,guid_models, bodyJson):
+    model_names = bodyJson['models'];
+    for model_name in model_names:
+      file_search = ifcfiles.get_specific_file(guid_models, model_name);
+      if(file_search != None):
+        file_search.transformCivilFile();          
     return 'Archivos procesados correctamente';
   
 # explicit function       
