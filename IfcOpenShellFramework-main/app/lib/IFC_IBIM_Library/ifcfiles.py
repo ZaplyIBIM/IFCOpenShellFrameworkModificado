4f2ddb82826ffd91a4f8dbd5eeacfccf5c9a8a44
AToradaIBIM <atorada@ibim.es>
2022-03-03 08:00:01 +0100

    REFACTOR - Renaming Variables IfcFiles

--- IfcOpenShellFramework-main/app/lib/IFC_IBIM_Library/ifcfiles.py
+++ IfcOpenShellFramework-main/app/lib/IFC_IBIM_Library/ifcfiles.py
@@ -9,36 +9,36 @@ import glob
 from .ifcfile import IfcFile
 
 
-class IfcFiles(list):
+class ifcfiles(list):
   #static variable
   ifcModelsLoaded = [];
 
   #ctor
-  def __new__(cls, urlFolder, guidModels):
-    isAlreadyOpen = next(filter(lambda ifcModels: ifcModels.url == urlFolder & ifcModels.guidFiles == guidModels, IfcFiles.ifcModelsLoaded), None);
-    if  isAlreadyOpen != None:
-      return isAlreadyOpen;
+  def __new__(cls, url_folder, guid_models):
+    is_already_open = next(filter(lambda ifcmodels: ifcmodels.url == url_folder & ifcmodels.guid_files == guid_models, ifcfiles.ifcModelsLoaded), None);
+    if  is_already_open != None:
+      return is_already_open;
     else:
-      return super(IfcFiles, cls).__new__(cls);
+      return super(ifcfiles, cls).__new__(cls);
 
   # ctor-initializer
-  def __init__(self,urlFolder, guidModels):
-    if not hasattr(self,'guidFiles'):
-      allIfcFilesPaths = glob.glob(f'{urlFolder}/*.ifc');
-      for ifcFilePath in allIfcFilesPaths:
-        ifcFileExtended = IfcFile(ifcFilePath);
-        self.append(ifcFileExtended);
-      self.guidFiles = guidModels;
-      self.url = urlFolder;
-      IfcFiles.ifcModelsLoaded.append(self);
+  def __init__(self,url_folder, guid_models):
+    if not hasattr(self,'guid_files'):
+      all_ifc_files_paths = glob.glob(f'{url_folder}/*.ifc');
+      for ifc_file_path in all_ifc_files_paths:
+        ifc_file_extended = IfcFile(ifc_file_path);
+        self.append(ifc_file_extended);
+      self.guid_files = guid_models;
+      self.url = url_folder;
+      ifcfiles.ifc_models_loaded.append(self);
 
   # methods 
-  def closeIfcs(guidModels): 
-    IfcFiles.ifcModelsLoaded = list(filter(lambda ifcModels: str(ifcModels.guidFiles) != guidModels, IfcFiles.ifcModelsLoaded))
+  def close_ifcs(guid_models): 
+    ifcfiles.ifc_models_loaded = list(filter(lambda ifcmodels: str(ifcmodels.guid_files) != guid_models, ifcfiles.ifcModelsLoaded))
 
-  def getSpecificFile(guidModels, fileName):
-    files = next(filter(lambda ifcModels: str(ifcModels.guidFiles) == guidModels, IfcFiles.ifcModelsLoaded), None);
+  def get_specific_file(guid_models, file_name):
+    files = next(filter(lambda ifcmodels: str(ifcmodels.guid_files) == guid_models, ifcfiles.ifcModelsLoaded), None);
     if  files != None:
-      return next(filter(lambda ifcFile: ifcFile.fileName == fileName, files), None);
+      return next(filter(lambda ifcfile: ifcfile.file_name == file_name, files), None);
 
   
